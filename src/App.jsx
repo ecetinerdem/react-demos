be718f0b47e7a094b514b0885132f85651da416b
Cetin Erdem <cetoerdem1@gmail.com>
2024-11-03 20:29:03 +0400

    add Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import { Accordions } from "./components/Accordions";
-import { accordionData } from "./utils/content";
+import { Form } from "./components/Form";
+
 
 
 
 //Get your components  under return statement and do not forget to import
 const App = () => {
   return (
-    {/* <div>
+    <>
+      {/* <div>
       <div className="accordion">
         {accordionData.map(({ title, content }) => (
           <Accordions title={title} content={content} />
         ))}
       </div>
     </div> */}
+    <Form />
+    </>
+
   );
 };
 
