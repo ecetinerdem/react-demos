7c42493d07835ef0a6f1808105a6aa9750530fed
Cetin Erdem <cetoerdem1@gmail.com>
2024-11-03 22:25:46 +0400

    add Nav and Products

--- src/Navigation/Navigation.jsx
+++ src/Navigation/Navigation.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './Nav.css'
+import { FiHeart } from 'react-icons/fi'
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from 'react-icons/ai'
 
 function Navigation() {
   return (
-    <div>Nav</div>
+    <nav>
+      <div className="nav-container">
+        <input className="seqarch-input" type="text" placeholder="Search..." />
+      </div>
+      <div className="profile-container">
+        <a href="#">
+          <FiHeart className="nav-icons" />
+        </a>
+        <a href="#">
+          <AiOutlineShoppingCart className="nav-icons" />
+        </a>
+        <a href="#">
+          <AiOutlineUserAdd className="nav-icons" />
+        </a>
+      </div>
+    </nav>
   )
 }
 
--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
 import './Products.css'
+import { AiFillStar } from 'react-icons/ai'
+import { BsFillBagHeartFill } from 'react-icons/bs'
 
 function Products() {
   return (
-    <div>
-      Products
-    </div>
+   <>
+   <section className="card-container">
+    <section className="card">
+      <img src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+      alt="Shoe"
+      className='card-img'
+      />
+      <div className="card-details">
+        <h3 className="card-title">Nike Air Monarch IV</h3>
+        <section className="card-reviews">
+          <AiFillStar className="rating-star" /><AiFillStar className="rating-star" /><AiFillStar className="rating-star" /><AiFillStar className="rating-star" />
+          <span className="total-reviews">(123 reviews)</span>
+        </section>
+        <section className="card-price">
+          <div className="price">
+            <del>$200.00</del>$140.00
+          </div>
+          <div className="bag">
+            <BsFillBagHeartFill className="bag-icon" />
+          </div>
+        </section>
+      </div>
+    </section>
+   </section>
+   </>
   )
 }
 
