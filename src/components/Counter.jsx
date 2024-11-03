5d509c972e55a5307399199cc5a695bedc71601c
Cetin Erdem <cetoerdem1@gmail.com>
2024-11-03 11:57:03 +0400

    add counter

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,7 +1,16 @@
 import React, { useState } from 'react'
+import '../style.css'
 
 const Counter = () => {
     const [count, setCount] = useState(0)
+
+    const increment = () => {
+        setCount(count + 1)
+    }
+
+    const decrement = () => {
+        setCount(count - 1)
+    }
   return (
     <div className='container'>
       <div>
