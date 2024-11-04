9068c5c02ba57cbecae1da7cf18725672506a56d
Cetin Erdem <cetoerdem1@gmail.com>
2024-11-04 11:46:37 +0400

    add sidebar details

--- src/Sidebar/Price/Price.jsx
+++ src/Sidebar/Price/Price.jsx
@@ -1,23 +1,44 @@
-import React from 'react'
-import './Price.css'
-import Input from '../../components/Input'
-
-function Price({handleChange}) {
+const Price = ({ handleChange }) => {
   return (
-    <div className='ml'>
-      <h2 className='sidebar-title price-title'>Price</h2>
+    <>
+      <div className="ml">
+        <h2 className="sidebar-title price-title">Price</h2>
+
         <label className="sidebar-label-container">
-          <input type="radio" onChange={handleChange} value="" name='test2' />
+          <input onChange={handleChange} type="radio" value="" name="test2" />
           <span className="checkmark"></span>All
         </label>
-      <Input handleChange={handleChange} value={'50'} title={'$0 - $50'} name={'test2'} color={'#f1f1f1'} />
-      <Input handleChange={handleChange} value={'100'} title={'$50 - $100'} name={'test2'} color={'#f1f1f1'} />
-      <Input handleChange={handleChange} value={'150'} title={'$100 - $150'} name={'test2'} color={'#f1f1f1'} />
-      <Input handleChange={handleChange} value={'200'} title={'$150 - $200'} name={'test2'} color={'#f1f1f1'} />
-      
 
-    </div>
-  )
-}
+        <Input
+          handleChange={handleChange}
+          value={50}
+          title="$0 - 50"
+          name="test2"
+        />
 
-export default Price
+        <Input
+          handleChange={handleChange}
+          value={100}
+          title="$50 - $100"
+          name="test2"
+        />
+
+        <Input
+          handleChange={handleChange}
+          value={150}
+          title="$100 - $150"
+          name="test2"
+        />
+
+        <Input
+          handleChange={handleChange}
+          value={200}
+          title="Over $150"
+          name="test2"
+        />
+      </div>
+    </>
+  );
+};
+
+export default Price;
