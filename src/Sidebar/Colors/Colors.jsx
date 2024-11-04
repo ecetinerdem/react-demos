7b69ca02840f6cc45b63055b4ba1f5ee82bd09bb
Cetin Erdem <cetoerdem1@gmail.com>
2024-11-04 10:17:21 +0400

    add color and price

--- src/Sidebar/Colors/Colors.jsx
+++ src/Sidebar/Colors/Colors.jsx
@@ -4,7 +4,19 @@ import './Colors.css'
 function Colors() {
   return (
     <div>
-      Colors
+        <label className='sidebar-label-container color title'>
+        <input type="radio" value="" name="test3" />
+        <span className="chekmark"></span>All
+      </label>
+      <label className='sidebar-label-container'>
+        <input type="radio" value="" name="test3" />
+        <span className="chekmark"></span>Blue
+      </label>
+      <label className='sidebar-label-container'>
+        <input type="radio" value="" name="test3" />
+        <span className="chekmark"></span>Red
+      </label>
+      
     </div>
 )
 
--- src/Sidebar/Price/Price.jsx
+++ src/Sidebar/Price/Price.jsx
@@ -3,7 +3,30 @@ import './Price.css'
 
 function Price() {
   return (
-    <div>Price</div>
+    <div className='ml'>
+      <h2 className='sidebar-title price-title'>Price</h2>
+      <label className='sidebar-label-container'>
+        <input type="radio" value="" name="test2" />
+        <span className="chekmark"></span>All
+      </label>
+      <label className='sidebar-label-container'>
+        <input type="radio" value="" name="test2" />
+        <span className="chekmark"></span>$0.00 - $50.00
+      </label>
+      <label className='sidebar-label-container'>
+        <input type="radio" value="" name="test2" />
+        <span className="chekmark"></span>$50.00 - $100.00
+      </label>
+      <label className='sidebar-label-container'>
+        <input type="radio" value="" name="test2" />
+        <span className="chekmark"></span>$100.00 - $150.00
+      </label>
+      <label className='sidebar-label-container'>
+        <input type="radio" value="" name="test2" />
+        <span className="chekmark"></span>$150.00 - $200.00
+      </label>
+
+    </div>
   )
 }
 
